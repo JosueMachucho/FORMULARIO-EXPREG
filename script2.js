ff30d69920a5f9b66e1b2ab898b62b98412e1e9f
JosueMachucho <72224100+JosueMachucho@users.noreply.github.com>
2020-10-29 11:56:26 -0500

    Initial commit

---
+++ script2.js
@@ -0,0 +1,9 @@
+var dato1 = localStorage["rfc"];
+var dato2 = localStorage["coordenadas"];
+
+
+var p1 = document.getElementById("d1");
+p1.innerHTML= dato1;
+var p2 = document.getElementById("d2");
+p2.innerHTML= dato2;
+
